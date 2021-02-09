fb29a5460302c1e25f5bdb3ef48c3f9eced9effa
vjiki <nezzabudka5@gmail.com>
2021-02-09 11:57:48 +0300

    first commit

---
+++ src/com/github/vjiki/Main.java
@@ -0,0 +1,25 @@
+package com.github.vjiki;
+
+public class Main {
+
+    public static void main(String[] args) {
+//        int i = 1;
+//        System.out.println(i);
+//        i = i + 1;
+//        System.out.println(i);
+//        i = i + 1;
+//        System.out.println(i);
+//        System.out.println("Hello World!");
+
+//        int a = 1;
+//        int b = 2;
+//        int c = a + b;
+//        System.out.println("sum = " + c);
+//        System.out.println("a > b = " + (a > b));
+//        System.out.println("a < b = " + (a < b));
+
+        String hello = "Hello";
+        String world = "world";
+        System.out.println(hello + " " + world + "!");
+    }
+}
